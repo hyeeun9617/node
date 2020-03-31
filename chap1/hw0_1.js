bda5716bf054c9dbe23896b7619be211637c5ecf
Lee, hyeeun <hyeeun9617@naver.com>
2020-03-31 20:53:34 +0900

    과제

--- chap1/hw0_1.js
+++
@@ -1,4 +0,0 @@
-let studentcount = 31;
-let student = 10;
-let answer = Math.ceil(studentcount/student);
-console.log(answer);
--- chap1/hw0_2.js
+++
@@ -1,6 +0,0 @@
-let max =20;
-let min =10;
-for (let i = 0; i<30; i++){ 
-    let a = Math.floor(Math.random()*(max-min+1)+min); 
-console.log(a);
-}
