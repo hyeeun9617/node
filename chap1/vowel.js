3013a20082d10ab588420c08af78cbdcfa564e34
hyeeun <hyeeun9617@naver.com>
2020-04-07 23:39:47 +0900

    모음위치 찾기

---
+++ chap1/vowel.js
@@ -0,0 +1,9 @@
+let s = "When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom, let it be.";
+
+for(let i = 0; i < s.length; i++){
+
+    if(s[i]=='a' || s[i]=='e'|| s[i]=='i'||s[i]=='o' || s[i]=='u'){
+        console.log("모음"+s[i]+" 의 위치는 "+i );
+    }
+    
+}
