882cacdda820c0e22de8b2065f6463cc55f9ac9b
hyeeun <hyeeun9617@naver.com>
2020-04-07 23:53:36 +0900

    모든 공백 제거

--- chap1/space.js
+++ chap1/space.js
@@ -1,3 +1,3 @@
-let s = "When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom of wisdom, let it be." ;
-let a = s.replace(/ /g, "");
+let s = "When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom, let it be." ;
+let a = s.replace(/ /gi, "");
 console.log(a);
