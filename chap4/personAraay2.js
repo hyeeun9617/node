c6318a115cbd0e8f36d207d4c3e8c0dfcd657fd9
hyeeun <hyeeun9617@naver.com>
2020-04-14 23:40:07 +0900

    4주차 과제 객체

--- chap4/personAraay2.js
+++ chap4/personAraay2.js
@@ -4,12 +4,10 @@ let persons2 = [];
 
 for(let i=0; i<10; i++){
     persons1[i] = {name: "홍길동", age : 16+i};
-    console.log(persons1[i]);
 }
 
 for(let i=0; i<10; i++){
     persons2[i] = {name: "홍길동", age : 16+i};
-    console.log(persons2[i]);
 }
 
 Object.assign(persons2, persons1);
