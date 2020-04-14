d60b2b044d3f0fa41d063ba7fee881f41a783c85
hyeeun <hyeeun9617@naver.com>
2020-04-14 23:26:14 +0900

    객체 복제

---
+++ chap4/personAraay2.js
@@ -0,0 +1,20 @@
+
+let persons1 = [];
+let persons2 = [];
+
+for(let i=0; i<10; i++){
+    persons1[i] = {name: "홍길동", age : 16+i};
+    console.log(persons1[i]);
+}
+
+for(let i=0; i<10; i++){
+    persons2[i] = {name: "홍길동", age : 16+i};
+    console.log(persons2[i]);
+}
+
+Object.assign(persons2, persons1);
+for(let i=0; i<10; i++){
+    console.log(persons2[i]);
+}
+
+
