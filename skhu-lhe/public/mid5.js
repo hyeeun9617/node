b37c3cdb121d7d3dad6067545264e0b4d59bdd62
hyeeunlee <hyeeun9617@naver.com>
2020-05-06 10:40:55 +0900

    test2

--- skhu-lhe/public/mid5.js
+++ skhu-lhe/public/mid5.js
@@ -1,10 +1,8 @@
 let a = [ "hello.html", "world.js", "readme.txt" ];
 
-for(let i=0;i<a.length;i++){
-   let str = a[i].split("");
-   
-   if(str == '.'){
-    console.log("v")
-   }
- }
+let str="g"
+let str2=t
+
+if(str!=str2)
+console.log("r")
 
